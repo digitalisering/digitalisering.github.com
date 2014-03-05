93f633a4c4a780b88c3beedf2f6d7f539457e510
MrThomasWeibel <weibel@outlook.com>
2014-03-05 08:30:14 +0100

    bum bare første upload

---
+++ db.js
@@ -0,0 +1,12 @@
+var beboere = [
+	{ "navn":"Aksel Bruun","id":"0","aft":"none"},
+	{ "navn":"Anne-Mette Bodilsen","id":"1","aft":"none"},
+	{ "navn":"Arne Nielsen","id":"2","aft":"none"},
+	{ "navn":"Inge Bording Andersen","id":"3","aft":"none"},
+	{ "navn":"Mads Skjern","id":"4","aft":"none"},
+	{ "navn":"Bodil Madsen","id":"5","aft":"none"},
+	{ "navn":"Dorte Mathiasen","id":"6","aft":"none"},
+	{ "navn":"Erik Jensen","id":"7","aft":"none"},
+	{ "navn":"Kirsten Rasmussen","id":"8","aft":"none"}
+]
+
