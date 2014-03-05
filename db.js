7935c7396a079920052ade78a9885f7d81cd2f6f
MrThomasWeibel <weibel@outlook.com>
2014-03-05 08:48:08 +0100

    opdateret verision

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 var beboere = [
-	{ "navn":"Aksel Bruun","id":"0","aft":"none"},
-	{ "navn":"Anne-Mette Bodilsen","id":"1","aft":"none"},
-	{ "navn":"Arne Nielsen","id":"2","aft":"none"},
-	{ "navn":"Inge Bording Andersen","id":"3","aft":"none"},
-	{ "navn":"Mads Skjern","id":"4","aft":"none"},
-	{ "navn":"Bodil Madsen","id":"5","aft":"none"},
-	{ "navn":"Dorte Mathiasen","id":"6","aft":"none"},
-	{ "navn":"Erik Jensen","id":"7","aft":"none"},
-	{ "navn":"Kirsten Rasmussen","id":"8","aft":"none"}
+	{ "navn":"Aksel Bruun","id":"0"},
+	{ "navn":"Anne-Mette Bodilsen","id":"1"},
+	{ "navn":"Arne Nielsen","id":"2"},
+	{ "navn":"Inge Bording Andersen","id":"3"},
+	{ "navn":"Mads Skjern","id":"4"},
+	{ "navn":"Bodil Madsen","id":"5"},
+	{ "navn":"Dorte Mathiasen","id":"6"},
+	{ "navn":"Erik Jensen","id":"7"},
+	{ "navn":"Kirsten Rasmussen","id":"8"}
 ]
 
